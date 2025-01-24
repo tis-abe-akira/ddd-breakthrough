d25da9031c701773c19a9be0a76e6b67c64eb0db
Akira Abe <akiraabe2005@yahoo.co.jp>
2025-01-25 08:24:33 +0900

    FacilityInvestmentServiceにcreateメソッドを追加し、投資ステータスをEXECUTEDに設定、processedDateを現在日時に更新

--- backend/src/main/java/com/syndicated_loan/syndicated_loan/common/dto/FacilityInvestmentDto.java
+++ backend/src/main/java/com/syndicated_loan/syndicated_loan/common/dto/FacilityInvestmentDto.java
@@ -21,13 +21,3 @@ public class FacilityInvestmentDto extends TransactionDto {
     private String investmentStatus; // 投資のステータス（COMMITTED, FUNDED など）
 }
 
-/*
- 
- date 2025-01-27T00:00:00
- amount 999
- positionId
- amountPieId
- status
- processedDate
-
- */
